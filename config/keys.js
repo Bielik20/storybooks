63b7d71efd389165292782b6ffd49a99e2b6ec9b
Bielik <bielik20@gmail.com>
2018-02-27 17:40:16 +0100

    Add handle Google OAuth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://bielik:bielik@ds151348.mlab.com:51348/storybooks-dev',
+  googleClientID: '757585670371-g5crkb07t3lobhbbg9b76n7s73hlct3g.apps.googleusercontent.com',
+  googleClientSecret: 'Altu12lT8JySlOx-Lo4BYB9M'
+}
