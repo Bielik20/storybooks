cb65f79f042144e08dada395a1fcac2dd768fa19
Bielik <bielik20@gmail.com>
2018-03-03 14:43:28 +0100

    Add Dev and Prod keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://bielik:bielik@ds151348.mlab.com:51348/storybooks-dev',
-  googleClientID: '757585670371-g5crkb07t3lobhbbg9b76n7s73hlct3g.apps.googleusercontent.com',
-  googleClientSecret: 'Altu12lT8JySlOx-Lo4BYB9M'
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');  
 }
